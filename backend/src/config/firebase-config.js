883ee7c86ca57006851f2520c82f95cf9419e671
Aaron Weldy <aweldy@umich.edu>
2021-08-07 17:24:10 -0700

    Add firebase env variable

--- backend/src/config/firebase-config.js
+++ backend/src/config/firebase-config.js
@@ -1,16 +1,6 @@
 import admin from "firebase-admin";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCXG6J4qNGuYc4P2RZM_wKAEdECO7_qaog",
-  authDomain: "ff-mern.firebaseapp.com",
-  projectId: "ff-mern",
-  storageBucket: "ff-mern.appspot.com",
-  messagingSenderId: "54859804653",
-  appId: "1:54859804653:web:7ca0c44cce579e08da01e4",
-  measurementId: "G-CKLBF5P572",
-};
-
-admin.initializeApp({ credential: admin.credential.applicationDefault() });
+admin.initializeApp({credential: admin.credential.cert(JSON.parse(Buffer.from(process.env.SERVICE_ACCOUNT, 'base64').toString('ascii')))});
 
 export default admin;
 export const db = admin.firestore();
