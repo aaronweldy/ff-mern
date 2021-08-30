2fcc7c6886975504bec6ee276c9886a00b77f30c
Aaron Weldy <aweldy@umich.edu>
2021-08-29 20:31:04 -0400

    Add larger limit for requests on server to accomodate full leagues

--- backend/src/lib/server.js
+++ backend/src/lib/server.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 
-import cors from "cors";
-import express from "express";
-import user from "../route/user.js";
-import league from "../route/league.js";
-import env from "dotenv";
+import cors from 'cors';
+import express from 'express';
+import user from '../route/user.js';
+import league from '../route/league.js';
+import env from 'dotenv';
 
 const app = express();
 env.config();
@@ -12,29 +12,29 @@ env.config();
 // env variables
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json(), cors());
+app.use(express.json({limit: '50mb'}), cors());
 
-app.get("/", (req, res) => {
-  console.log("receiving message");
-  res.json({ message: "API working" });
+app.get('/', (req, res) => {
+	console.log('receiving message');
+	res.json({ message: 'API working' });
 });
 
-app.use("/api/v1/user/", user);
-app.use("/api/v1/league/", league);
+app.use('/api/v1/user/', user);
+app.use('/api/v1/league/', league);
 
-app.all("*", (request, response) => {
-  console.log("Returning a 404 from the catch-all route");
-  return response.sendStatus(404);
+app.all('*', (request, response) => {
+	console.log('Returning a 404 from the catch-all route');
+	return response.sendStatus(404);
 });
 
 export const start = () => {
-  app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`);
-  });
+	app.listen(PORT, () => {
+		console.log(`Listening on port: ${PORT}`);
+	});
 };
 
 export const stop = () => {
-  app.close(PORT, () => {
-    console.log(`Shut down on port: ${PORT}`);
-  });
+	app.close(PORT, () => {
+		console.log(`Shut down on port: ${PORT}`);
+	});
 };
